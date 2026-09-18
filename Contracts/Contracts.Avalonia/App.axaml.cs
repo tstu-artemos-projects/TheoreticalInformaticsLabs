5e1c9f2e4f6f873a1901994dbf8451cd333326c0
Urtyom-Alyanov <urtyomalyanov@gmail.com>
2026-09-18 21:03:33 +0300

    chore: add dependency injection and mvvm community tools

--- Contracts/Contracts.Avalonia/App.axaml.cs
+++ Contracts/Contracts.Avalonia/App.axaml.cs
@@ -1,13 +1,35 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Contracts.Avalonia;
 
+/// <summary>
+/// Логика для App.axaml
+/// </summary>
 public partial class App : Application
 {
+  public static IServiceProvider ServiceProvider { get; private set; } = null!;
+
+  /// <summary>
+  /// Конфигурация и инъекция сервисов сервисов
+  /// </summary>
+  /// <param name="services">Объект `IServiceCollection`</param>
+  private void _configureServices(IServiceCollection services)
+  {
+    services.AddTransient<MainWindow>();
+  }
+
   public override void Initialize()
   {
+    var services = new ServiceCollection();
+
+    _configureServices(services);
+
+    ServiceProvider = services.BuildServiceProvider();
+
     AvaloniaXamlLoader.Load(this);
   }
 
@@ -15,7 +37,8 @@ public partial class App : Application
   {
     if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
     {
-      desktop.MainWindow = new MainWindow();
+      var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+      desktop.MainWindow = mainWindow;
     }
 
     base.OnFrameworkInitializationCompleted();
---
+++ Contracts/Contracts.Avalonia/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Contracts.Avalonia.Converters;
+
+/// <summary>
+/// Определяет булево значение в зависимости от
+/// того, содержит ли значение нулевой указатель
+/// </summary>
+public class NullToBooleanConverter : IValueConverter
+{
+  public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    => value != null;
+
+  public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    => throw new NotImplementedException();
+}

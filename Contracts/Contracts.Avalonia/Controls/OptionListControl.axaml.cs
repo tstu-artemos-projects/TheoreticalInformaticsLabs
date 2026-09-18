7feb7e7645d570d2df50efc565fa73c945cb2c4a
Urtyom-Alyanov <urtyomalyanov@gmail.com>
2026-09-18 21:50:09 +0300

    ui: add controls

---
+++ Contracts/Contracts.Avalonia/Controls/OptionListControl.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Contracts.Avalonia.Controls;
+
+public partial class OptionListControl : UserControl
+{
+  public OptionListControl()
+  {
+    InitializeComponent();
+  }
+}
+
